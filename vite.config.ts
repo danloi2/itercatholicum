6740138597d98c6a946dc4714147a8c44776abba
Daniel Losada <daniel.losada@ehu.eus>
2026-03-11 23:18:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VERCEL ? "/" : "/itercatholicum/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
